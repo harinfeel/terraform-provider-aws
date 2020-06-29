8f0731513caff43a0a2e832de8f749e9c9fd3236
harin <harin@megazone.com>
2020-06-29 13:25:59 +0900

    Change AWS AMIs!

--- examples/two-tier/variables.tf
+++ examples/two-tier/variables.tf
@@ -20,7 +20,10 @@ variable "aws_region" {
 # Centos 7
 variable "aws_amis" {
   default = {
-    #us-east-1 = "ami-06e83aceba2cb0907"
-    ap-northeast-2 = "ami-06e83aceba2cb0907"
+    eu-west-1 = "ami-0d4002a13019b7703"
+    us-east-1 = "ami-06cf02a98a61f9f5e"
+    us-west-1 = "ami-02676464a065c9c05"
+    us-west-2 = "ami-0a248ce88bcc7bd23"
+    ap-northeast-2 = "ami-0cf8e67d10c823f2e"
   }
 }
